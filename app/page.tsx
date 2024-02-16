98853964c8aed295d197a28edbf9fe337b152766
Vinícios Barbosa <viniciosbarbosa740@gmail.com>
2024-02-16 10:29:03 -0300

    refactor: :iphone: improve mobile ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CardMain from './components/CardMain'
 
 export default function Home() {
   return (
-    <main className="p-10">
+    <main className="p-4 lg:p-10">
       <div className="w-100 lg:max-w-[1024px] flex flex-col lg:flex-row mx-auto gap-4">
         <div className="w-100 md:max-w-[360px] space-y-4">
           <CardMain />
