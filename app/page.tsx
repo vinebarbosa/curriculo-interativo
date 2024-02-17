075e839bc50c87cb54108dc765648d43ec79f247
Vinícios Barbosa <vinicios.barbosa@lais.huol.ufrn.br>
2024-02-16 22:25:50 -0300

    refactor: :iphone: add experience section to home page and improve mobile ux

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import AboutCard from './components/AboutCard'
 import MainInfoCard from './components/MainInfoCard'
 import InformationCard from './components/InformationCard'
+import ExperienceSection from './components/ExperienceSection'
 
 export default function Home() {
   return (
-    <main className="p-4 lg:p-10">
-      <div className="w-100 lg:max-w-[1024px] flex flex-col lg:flex-row mx-auto gap-4">
-        <div className="w-100 md:max-w-[360px] space-y-4">
+    <main className="p-4 md:p-10">
+      <div className="w-100 max-w-100 md:max-w-[400px] lg:max-w-[1024px] flex flex-col lg:flex-row mx-auto gap-4">
+        <div className="w-full lg:w-[600px] space-y-4">
           <MainInfoCard />
           <AboutCard />
           <InformationCard />
         </div>
-        <div></div>
+        <div className="w-full">
+          <ExperienceSection />
+        </div>
       </div>
     </main>
   )
